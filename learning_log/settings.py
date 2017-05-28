6506d5e97f7ab6c77e1df2213fe30dfb73a6587b
yosepppph <yosephhailemariam@gmail.com>
2017-05-27 20:10:48 -0400

    Added custom 404 and 500 error pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@qxy8*ysry8s#%fir4k8@6(u10*n-thslox4pi1_h67j!q!qa3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -93,7 +93,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
